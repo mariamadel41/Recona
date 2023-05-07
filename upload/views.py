109143f924902664cdb5a908f71fee0dc7dff75a
Mariam <mariamadel9420@gmail.com>
2023-05-07 18:41:44 +0300

    take file from user

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import UploadForm
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'upload/success.html')
+    else:
+        form = UploadForm()
+    return render(request, 'upload/upload.html', {'form': form})
